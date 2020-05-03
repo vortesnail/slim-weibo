f512eabea7f1daa4824fec6bf07a0a7e2a6bc058
vortesnail <1091331061@qq.com>
2020-05-03 23:51:28 +0800

    feature: router demo

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,13 +6,26 @@ router.get('/', async (ctx, next) => {
   })
 })
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
-
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
+  }
+})
+
+router.get('/profile/:userName', async (ctx, next) => {
+  const { userName } = ctx.params
+  ctx.body = {
+    title: 'this is profile page',
+    userName
+  }
+})
+
+router.get('/loadMore/:userName/:pageIndex', async (ctx, next) => {
+  const { userName, pageIndex } = ctx.params
+  ctx.body = {
+    title: 'this is loadMore api',
+    userName,
+    pageIndex
   }
 })
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -10,4 +10,12 @@ router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
 
+router.post('/login', async (ctx, next) => {
+  const { userName, password } = ctx.request.body
+  ctx.body = {
+    userName,
+    password
+  }
+})
+
 module.exports = router
