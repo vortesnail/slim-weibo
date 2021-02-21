088b45b822e1177511e1e7df6c3f91e881f027b0
vortesnail <1091331061@qq.com>
2021-02-21 23:33:05 +0800

    test: 一些路由测试

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,13 +6,26 @@ router.get('/', async (ctx, next) => {
   })
 })
 
-router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
-
 router.get('/json', async (ctx, next) => {
   ctx.body = {
     title: 'koa2 json'
+  }
+})
+
+router.get('/profile/:userName', async (ctx, next) => {
+  const { userName } = ctx.params
+  ctx.body = {
+    title: 'This is a profile page',
+    userName
+  }
+})
+
+router.get('/loadMore/:userName/:pageIndex', async (ctx, next) => {
+  const { userName, pageIndex } = ctx.params
+  ctx.body = {
+    title: 'This is a loadMore API',
+    userName,
+    pageIndex
   }
 })
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -10,4 +10,13 @@ router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
 
+router.post('/login', async (ctx, next) => {
+  const { userName, password } = ctx.request.body
+  ctx.body = {
+    err: 0,
+    userName,
+    password
+  }
+})
+
 module.exports = router
