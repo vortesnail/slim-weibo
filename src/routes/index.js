b527a3d63bfcc0c46c3a17d6af6469494b99c649
vortesnail <1091331061@qq.com>
2021-02-22 20:40:49 +0800

    feat: ejs 演示

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,20 @@ const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
+    title: 'Hello Koa 2!',
+    isMe: true,
+    blogList: [
+      {
+        id: 1,
+        title: 'aaa',
+      }, {
+        id: 2,
+        title: 'bbb',
+      }, {
+        id: 3,
+        title: 'ccc',
+      }
+    ]
   })
 })
 
