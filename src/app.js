742df596ad45cdd94a893c10f1ccc3953493aecf
vortesnail <1091331061@qq.com>
2021-02-24 22:33:34 +0800

    feat: 增加 session 配置

--- src/app.js
+++ src/app.js
@@ -5,6 +5,10 @@ const json = require('koa-json')
 const onerror = require('koa-onerror')
 const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
+const session = require('koa-generic-session')
+const redisStore = require('koa-redis')
+
+const { REDIS_CONF } = require('./conf/db')
 
 const index = require('./routes/index')
 const users = require('./routes/users')
@@ -22,6 +26,22 @@ app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
   extension: 'ejs'
+}))
+
+// session 配置
+app.keys = ['VoRteSNail_*&(#']
+app.use(session({
+  key: 'weibo.sid', // cookie name 默认是 `koa.sid`
+  prefix: 'weibo:sess:', // redis key 的前缀，默认是 `koa:sess:`
+  cookie: {
+    path: '/',
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000, // 单位 ms
+  },
+  store: redisStore({
+    host: REDIS_CONF.host,
+    port: REDIS_CONF.port,
+  }),
 }))
 
 // logger
