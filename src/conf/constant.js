7f18d456cb3aebb361822404a18ec780008ce959
vortesnail <1091331061@qq.com>
2021-02-28 16:27:23 +0800

    feat: 增加 setting 页面 ejs 模板

--- src/conf/constant.js
+++ src/conf/constant.js
@@ -4,5 +4,5 @@
  */
 
 module.exports = {
-  DEFAULT_AVATAR: 'http://dwz.cn/rnTnftZs',
+  DEFAULT_AVATAR: 'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=1517417813,2367413112&fm=26&gp=0.jpg',
 }
