1484a55f17e349898c1a5001e9a97a7400e3b8e9
vortesnail <1091331061@qq.com>
2021-02-28 21:20:01 +0800

    feat: 用户设置单元测试

--- test/user/login.test.js
+++ test/user/login.test.js
@@ -65,10 +65,43 @@ it('登录，应该成功', async () => {
   COOKIE = res.headers['set-cookie'].join(';')
 })
 
+// 修改基本信息
+it('修改基本信息，应该成功', async () => {
+  const res = await server
+    .patch('/api/user/changeInfo')
+    .send({
+      nickName: '测试昵称',
+      city: '测试城市',
+      avatar: '/test.png'
+    })
+    .set('cookie', COOKIE)
+  expect(res.body.errno).toBe(0)
+})
+
+// 修改密码
+it('修改密码，应该成功', async () => {
+  const res = await server
+    .patch('/api/user/changePassword')
+    .send({
+      password,
+      newPassword: `u_${Date.now()}`
+    })
+    .set('cookie', COOKIE)
+  expect(res.body.errno).toBe(0)
+})
+
 // 删除
 it('删除用户，应该成功', async () => {
   const res = await server
     .post('/api/user/delete')
+    .set('cookie', COOKIE)
+  expect(res.body.errno).toBe(0)
+})
+
+// 退出
+it('退出登录，应该成功', async () => {
+  const res = await server
+    .post('/api/user/logout')
     .set('cookie', COOKIE)
   expect(res.body.errno).toBe(0)
 })
