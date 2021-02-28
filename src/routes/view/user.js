dcf4ba3662f7648aabb63b1fd3e74e2b832ca3d9
vortesnail <1091331061@qq.com>
2021-02-28 13:13:01 +0800

    fix: 已登陆情况下的页面状态修正

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -5,12 +5,32 @@
 
 const router = require('koa-router')()
 
+/**
+ * 获取登录信息
+ * @param {object} ctx koa2 ctx
+ */
+function getLoginInfo(ctx) {
+  let data = {
+    isLogin: false,
+  }
+
+  const userInfo = ctx.session.userInfo
+  if (userInfo) {
+    data = {
+      isLogin: true,
+      userName: userInfo.userName
+    }
+  }
+
+  return data
+}
+
 router.get('/login', async (ctx, next) => {
-  await ctx.render('login', {})
+  await ctx.render('login', getLoginInfo(ctx))
 })
 
 router.get('/register', async (ctx, next) => {
-  await ctx.render('register', {})
+  await ctx.render('register', getLoginInfo(ctx))
 })
 
 module.exports = router
