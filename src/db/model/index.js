2a729726e96eb52274783da7a7f14f5903c784ed
vortesnail <1091331061@qq.com>
2021-03-03 22:06:42 +0800

    feat: 粉丝数据模型

--- src/db/model/index.js
+++ src/db/model/index.js
@@ -5,13 +5,23 @@
 
 const User = require('./User')
 const Blog = require('./Blog')
+const UserRelation = require('./UserRelation')
 
 // 查询微博时顺带查出用户
 Blog.belongsTo(User, {
   foreignKey: 'userId'
 })
 
+UserRelation.belongsTo(User, {
+  foreignKey: 'followerId'
+})
+
+User.hasMany(UserRelation, {
+  foreignKey: 'userId'
+})
+
 module.exports = {
   User,
   Blog,
+  UserRelation,
 }
